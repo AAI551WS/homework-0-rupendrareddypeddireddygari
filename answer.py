334686933be5622f75a4b116944d333a70f89564
rupendrareddypeddireddygari <157648794+rupendrareddypeddireddygari@users.noreply.github.com>
2024-01-24 17:20:33 -0500

    homework-0/docstring/display message/output of a function

--- answer.py
+++ answer.py
@@ -2,16 +2,19 @@ def hello_world():
     # Write a doc string below. Doc strings can be used to provide the user with a
     # description of what the function does.
     """
-
+    Function dispalys a welcome note to the users
+    Returns :
+    string : Welcome, ${user}
+    
     """
 
     # Write a comment; comments are very useful to other programmers.
-
+    # Hash symbol is used at the beginning of the statement to represent a comment
     # Write a Python statement to print Hello, World!
-
+    print("Hello, World!")
 def add(a,b):
 
     # Write simple code to add variable a and b
-    output =
+    output = a + b 
 
     return output
